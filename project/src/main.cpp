b53376e96cd9c95238e55647ce60da1a54678a24
unknown <rozanski.michal.94@gmail.com>
2020-09-15 17:04:41 +0200

    Add cmake to compile

--- project/src/main.cpp
+++ project/src/main.cpp
@@ -1,4 +1,6 @@
 #include <string>
+#include <iostream>
+
 
 #include "koh.hpp"
 #include "pngWriter.hpp"
@@ -28,25 +30,30 @@ struct Parameters
 Parameters parseArgs(int args, char argv[]);
 
 
-int main(int args, char argv[])
+int main(int args, char* argv[])
 {
+	int *a = nullptr;
+	std::cout << "HellowWord" << std::endl;
+
 	// Parse arguments
-	Parameters params = parseArgs(args, argv);
+	// Parameters params = parseArgs(args, argv);
 
-	// Read Png
-	PngMode mode = PngMode::GREY;
-	PngReader pngReader = PngReader();
-	pngReader.open(params.path, mode);
-	Image startImage = pngReader.getImage(params.frameSize);
+	// // Read Png
+	// PngMode mode = PngMode::GREY;
+	// PngReader pngReader = PngReader();
+	// pngReader.open(params.path, mode);
+	// Image startImage = pngReader.getImage(params.frameSize);
 
-	// training Kohonen network
-	auto numberOfInputs = params.frameSize.x + params.frameSize.y;
-	Koh koh = Koh(params.koh.size, params.koh.learningRate, params.koh.epochs, numberOfInputs);
-	koh.train(startImage, params.numberOftrainSamples);
-	Image newImage = koh.convert(startImage);
+	// // training Kohonen network
+	// auto numberOfInputs = params.frameSize.x + params.frameSize.y;
+	// Koh koh = Koh(params.koh.size, params.koh.learningRate, params.koh.epochs, numberOfInputs);
+	// koh.train(startImage, params.numberOftrainSamples);
+	// Image newImage = koh.convert(startImage);
 
 
-	//saving png created by kohonen
-	PngWriter pngWriter = PngWriter();
-	pngWriter.save(params.outPath, newImage, mode);
+	// //saving png created by kohonen
+	// PngWriter pngWriter = PngWriter();
+	// pngWriter.save(params.outPath, newImage, mode);
+
+	return 0;
 }
--- project/src/png/pngWriter.hpp
+++ project/src/png/pngWriter.hpp
@@ -1,6 +1,7 @@
 #ifndef PNG_WRITER_HPP
 #define PNG_WRITER_HPP
 
+#include "pngMode.hpp"
 
 class PngWriter
 {
@@ -8,6 +9,6 @@ private:
 public:
 	void save(unsigned char path[], Image image, PngMode mode);
 	void save(std::string path, Image image, PngMode mode);
-}
+};
 
 #endif //PNG_WRITER_HPP
